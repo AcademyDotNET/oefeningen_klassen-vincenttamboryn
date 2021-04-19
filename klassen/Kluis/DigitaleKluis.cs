56d80a4de98e2095a33fc5d372ad127ab752776e
vincent tamboryn <vincent.tamboryn@hotmail.com>
2021-04-19 11:51:22 +0200

    Last excercise, digital safe

---
+++ klassen/Kluis/DigitaleKluis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kluis
+{
+    class DigitaleKluis
+    {
+        private int code;
+        private bool canShowCode;
+        private int cheats = 0;
+        private int tries = 0;
+        public DigitaleKluis(int passnumber)
+        {
+            code = passnumber;
+        }
+        
+        public bool CanShowCode
+        {
+            get { return canShowCode; }
+            set { canShowCode = value; }
+        }
+        public int CodeLevel
+        {
+            get { return (code/1000); }
+        }
+        public int Code
+        {
+            get 
+            {
+                if (CanShowCode)
+                {
+                    return code;
+                }
+                else
+                {
+                    cheats++;
+                    return -666;
+                }
+            }
+            private set { code = value; }
+        }
+        public bool Trycode(int guess)
+        {
+            tries++;
+            if (guess == code)
+            {
+                Console.WriteLine($"You guessed correctly! {code} was the correct code! It only took you {tries} guesses");
+                if (cheats > 0)
+                {
+                    Console.WriteLine($"You are a cheater, so this achievement is mostly meaningless");
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"You guessed incorrectly. {guess} was not correct code. {tries} guesses so far");
+                return false;
+            }
+        }
+    }
+}
---
+++ klassen/Kluis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Kluis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitaleKluis kluis1 = new DigitaleKluis(3546);
+            Console.WriteLine($"this safe is of level {kluis1.CodeLevel}");
+            kluis1.Trycode(3756);
+            kluis1.Trycode(3541);
+            kluis1.Trycode(3256);
+            kluis1.Trycode(3416);
+            kluis1.Trycode(3546);
+
+            DigitaleKluis kluis2 = new DigitaleKluis(3646);
+            Console.WriteLine($"this safe is of level {kluis2.CodeLevel}");
+            kluis2.Trycode(3756);
+            kluis2.Trycode(3541);
+            kluis2.Trycode(3256);
+            kluis2.Trycode(3416);
+            int a = kluis2.Code;
+            Console.WriteLine($"is the code {a}?");
+            kluis2.Trycode(a);
+            kluis2.Trycode(3646);
+
+            DigitaleKluis kluis3 = new DigitaleKluis(3646);
+            Console.WriteLine($"this safe is of level {kluis3.CodeLevel}");
+            kluis3.Trycode(3756);
+            kluis3.Trycode(3541);
+            kluis3.Trycode(3256);
+            kluis3.Trycode(3416);
+            kluis3.CanShowCode = true;
+            int b = kluis3.Code;
+            Console.WriteLine($"is the code {b}?");
+            kluis3.Trycode(b);
+        }
+    }
+}
